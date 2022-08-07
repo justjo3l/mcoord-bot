ae495b7016b68fdc1a50773d211b6840d46fd21d
justjo3l <comedyandcomputers@gmail.com>
2022-08-08 00:24:27 +1000

    Changing files to store token as environment variable

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import hikari
+import os
 
 f = open('ev.txt', 'r')
 token = f.readline()
 
-bot = hikari.GatewayBot(token=token)
+bot = hikari.GatewayBot(token=os.environ.get("BOT_TOKEN"))
 bot.run()
